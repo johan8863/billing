b50407b7f7680af9b133743a0d89ae64be4a6f96
Johan Travieso Castro <jtravieso8863@gmail.com>
2023-04-05 23:22:06 -0400

    home view

--- project/urls.py
+++ project/urls.py
@@ -2,8 +2,10 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views import generic
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', generic.TemplateView.as_view(template_name='home.html')),
     path('kits/', include('apps.kits.urls')),
 ]
