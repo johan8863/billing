821e79b6c6535ae459436d072fe7bd7935943690
Johan Travieso Castro <jtravieso8863@gmail.com>
2023-04-06 22:11:44 -0400

    create view

--- apps/kits/urls.py
+++ apps/kits/urls.py
@@ -9,5 +9,6 @@ from . import views
 app_name = 'kits'
 urlpatterns = [
     path('list/', views.KitList.as_view(), name='list_kits'),
+    path('add/', views.KitCreate.as_view(), name='create_kit'),
     path('update/<int:pk>/', views.KitUpdate.as_view(), name='update_kit'),
 ]
--- apps/kits/views.py
+++ apps/kits/views.py
@@ -9,13 +9,22 @@ from .models import Kit
 
 
 class KitList(generic.ListView):
-    """class to list kits objects"""
+    """class for listing kits objects"""
     model = Kit
     paginate_by = 8
     template_name = 'kits/list.html'
 
 
+class KitCreate(generic.CreateView):
+    """Class for creating Kit objects"""
+    model = Kit
+    fields = ['name']
+    template_name = 'kits/create.html'
+    success_url = reverse_lazy('kits:list_kits')
+
+
 class KitUpdate(generic.UpdateView):
+    """Class for updating Kit objects"""
     model = Kit
     context_object_name = 'kit'
     fields = ['name']
