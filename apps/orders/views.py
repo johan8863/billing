f222f0d21ed4e77a06b1c8d23fac83aabd2c3409
Johan Travieso Castro <jtravieso8863@gmail.com>
2023-05-08 17:59:29 -0400

    checking formset errors

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -83,20 +83,6 @@ class OrderUpdate(generic.UpdateView):
 
         if form.is_valid() and formset.is_valid():
             print('valid')
-            # order = Order(**form.cleaned_data)
-            # order.save()
-
-            # for form in formset:
-            #     if form.cleaned_data.get('item'):
-            #         item = form.cleaned_data.get('item')
-            #         times = form.cleaned_data.get('times')
-            #         item_times = ItemTimes(
-            #             item = item,
-            #             order = order,
-            #             times = times,
-            #         )
-            #         item_times.save()
-            
             return redirect(self.success_url)
         else:
             print(formset.errors)
