13ec365bfffacd17b71381bbc7a61191d251ed8e
Johan Travieso Castro <jtravieso8863@gmail.com>
2023-05-09 12:08:04 -0400

    update and create post functions improved

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -26,7 +26,7 @@ class OrderCreate(generic.CreateView):
     success_url = reverse_lazy('orders:list')
 
     def get(self, request):
-        ItemTimesFormSet = modelformset_factory(ItemTimes, exclude=['order'], extra=5)
+        ItemTimesFormSet = modelformset_factory(ItemTimes, exclude=('order',), extra=5)
         formset = ItemTimesFormSet(queryset=ItemTimes.objects.none())
 
         return render(request, self.template_name, {
@@ -36,23 +36,21 @@ class OrderCreate(generic.CreateView):
     
     def post(self, request):
         form = self.form_class(request.POST)
-        ItemTimesFormSet = modelformset_factory(ItemTimes, exclude=['order'], extra=5)
+        ItemTimesFormSet = modelformset_factory(ItemTimes, exclude=('order',), extra=5)
         formset = ItemTimesFormSet(request.POST)
 
         if form.is_valid() and formset.is_valid():
-            order = Order(**form.cleaned_data)
-            order.save()
+            order = form.save()
 
             for form in formset:
                 if form.cleaned_data.get('item'):
                     item = form.cleaned_data.get('item')
                     times = form.cleaned_data.get('times')
-                    item_times = ItemTimes(
+                    ItemTimes.objects.create(
                         item = item,
                         order = order,
                         times = times,
                     )
-                    item_times.save()
             
             return redirect(self.success_url)
 
@@ -75,14 +73,27 @@ class OrderUpdate(generic.UpdateView):
             'formset': formset
         })
     
-    def post(self, request, *args, **kwargs):
+    def post(self, request, **kwargs):
         order = Order.objects.get(pk=kwargs['pk'])
-        form = self.form_class(request.POST)
-        ItemTimesFormSet = modelformset_factory(ItemTimes, exclude=['order'], extra=5)
-        formset = ItemTimesFormSet(request.POST)
+        item_times = ItemTimes.objects.filter(order=order)
+        form = self.form_class(request.POST, instance=order)
+        ItemTimesFormSet = modelformset_factory(ItemTimes, exclude=('order',), extra=5)
+        formset = ItemTimesFormSet(request.POST, queryset=item_times)
 
         if form.is_valid() and formset.is_valid():
-            print('valid')
+            form.save()
+            order.items.clear()
+
+            for form in formset:
+                if form.cleaned_data.get('item'):
+                    item = form.cleaned_data.get('item')
+                    times = form.cleaned_data.get('times')
+                    ItemTimes.objects.create(
+                        item = item,
+                        order = order,
+                        times = times,
+                    )
+
             return redirect(self.success_url)
         else:
             print(formset.errors)
